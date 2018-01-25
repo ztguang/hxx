d560c0bb2e07d9a6613cd63323a45ab0155dd5c8
张同光 <jsjoscpu@163.com>
2018-01-25 11:23:27 +0800

    Update hxx_baumwelch-1.cpp

--- src/hxx_baumwelch-1.cpp
+++ src/hxx_baumwelch-1.cpp
@@ -1,6 +1,6 @@
 /*
 [root@bupt src]# make
-[root@bupt src]# ./hxx_baumwelch-1 0.5 0.5 0.5 0.5 0.75 0.25 0.25 0.75 0.1 0.9 6
+[root@bupt src]# ./hxx_baumwelch-1   0.5 0.5 0.5 0.5   0.75 0.25 0.25 0.75   0.1 0.9   6
 */
 
 #include "hxx.hpp"
